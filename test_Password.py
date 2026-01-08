36d95e63cb946c8c2515c69fe8ae8820b8068da6
Nagaraj Kuntoji <kn733478@gmail.com>
2026-01-08 13:11:56 +0530

    Update test_Password.py

---
+++ test_Password.py
@@ -0,0 +1,22 @@
+import pytest
+from password import password_strength
+
+
+def test_strong_password():
+    output = password_strength("Nagaraj", "Strong@123")
+    assert "Strength: STRONG" in output
+
+
+def test_medium_password():
+    output = password_strength("Ravi", "Password1")
+    assert "Strength: MEDIUM" in output
+
+
+def test_weak_password_common():
+    output = password_strength("Arun", "password")
+    assert "Password is too common and unsafe." in output
+
+
+def test_missing_special_character():
+    output = password_strength("Suresh", "Strong123")
+    assert "Add at least one special character." in output
